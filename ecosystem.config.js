5e09d18f87a54db80dc02048bae7f3930ba9a491
TriTruong666 <tritruonghoang3@gmail.com>
2025-02-16 01:12:37 +0700

    add config

---
+++ ecosystem.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  apps: [],
+  deploy: {
+    production: {
+      user: "admin-picare",
+
+      host: ["192.168.84.81"],
+
+      ref: "origin/main",
+
+      repo: "git@github.com:TriTruong666/Loyalty-FE.git",
+
+      path: "/var/frontends/loyaltyfe",
+
+      "post-setup": "npm install",
+
+      "post-deploy": "pm2 startOrRestart ecosystem.config.js --env production",
+    },
+  },
+};
