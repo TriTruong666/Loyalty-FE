64957b1e0f5fbd6690dac053011ceb98904f5cbf
TriTruong666 <tritruonghoang3@gmail.com>
2025-02-15 17:17:29 +0700

    Begin at 15/02/2025

---
+++ tailwind.config.ts
@@ -0,0 +1,32 @@
+import { heroui } from "@heroui/theme";
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ["var(--font-inter)"],
+        open: ["var(--font-open-sans)"],
+        monse: ["var(--font-monse)"],
+      },
+      borderColor: {
+        "gray-400-40": "rgba(117,117,117, 0.2)",
+      },
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        primary: "var(--primary)",
+        normal: "var(--normal)",
+        dangerous: "var(--dangerous)",
+      },
+    },
+  },
+  darkMode: "class",
+  plugins: [heroui()],
+} satisfies Config;
