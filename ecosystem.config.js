f299ee8a3924f8c8af97c50161248c27cb140ef1
Sakamoto2004 <phbtoan9185@gmail.com>
2025-02-16 12:38:29 +0700

    Fix pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,20 +1,16 @@
 module.exports = {
-  apps: [],
-  deploy: {
-    production: {
-      user: "admin-picare",
-
-      host: ["192.168.84.81"],
-
-      ref: "origin/main",
-
-      repo: "git@github.com:TriTruong666/Loyalty-FE.git",
-
-      path: "/var/frontends/loyaltyfe",
-
-      "post-setup": "npm install && npm run build",
-
-      "post-deploy": "npm run start && pm2 reload loyalty-demo",
+    apps: [],
+    deploy: {
+        production: {
+            user: "admin-picare",
+            host: ["192.168.84.81"],
+            ref: "origin/main",
+            repo: "git@github.com:TriTruong666/Loyalty-FE.git",
+            path: "/var/frontends/loyaltyfe",
+            "pre-setup": "rm -r /var/frontends/loyaltyfe/*",
+            "post-setup": "npm install",
+            "post-deploy": "npm run build && pm2 start npm --name Loyalty-Frontend -- start",
+        },
     },
-  },
 };
+
