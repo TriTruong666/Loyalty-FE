8328aa33ecdcc661d77adbdcda01d1f8c5670dd3
TriTruong666 <tritruonghoang3@gmail.com>
2025-02-16 23:24:07 +0700

    Revert commit

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,8 +9,7 @@ module.exports = {
       path: "/var/frontends/loyaltyfe",
       "pre-setup": "rm -r /var/frontends/loyaltyfe/*",
       "post-setup": "npm install",
-      "pre-deploy":
-        "npm i && pm2 start npm --name Loyalty-Frontend -- start && pm2 delete Loyalty-Frontend",
+      "pre-deploy": "pm2 delete Loyalty-Frontend",
       "post-deploy":
         "npm run build && pm2 start npm --name Loyalty-Frontend -- start",
     },
