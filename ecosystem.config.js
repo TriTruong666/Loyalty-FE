29503c22d066fe52233fa480054f27657ab28eca
Sakamoto2004 <phbtoan9185@gmail.com>
2025-02-17 00:18:57 +0700

    remove rm -rf from ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,6 @@ module.exports = {
       ref: "origin/main",
       repo: "git@github.com:TriTruong666/Loyalty-FE.git",
       path: "/var/frontends/loyaltyfe",
-      "pre-setup": "rm -r /var/frontends/loyaltyfe/*",
       "post-setup": "npm install && npm run build",
       "pre-deploy": "pm2 start npm --name Loyalty-Frontend -- start && pm2 delete Loyalty-Frontend",
       "post-deploy": "npm i && npm run build && pm2 start npm --name Loyalty-Frontend -- start",
