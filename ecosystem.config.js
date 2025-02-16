5f8b4234ed079aa3737f7ba2ab770427becffd68
TriTruong666 <tritruonghoang3@gmail.com>
2025-02-16 23:20:37 +0700

    test thu

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,17 +1,18 @@
 module.exports = {
-    apps: [],
-    deploy: {
-        production: {
-            user: "admin-picare",
-            host: ["192.168.84.81"],
-            ref: "origin/main",
-            repo: "git@github.com:TriTruong666/Loyalty-FE.git",
-            path: "/var/frontends/loyaltyfe",
-            "pre-setup": "rm -r /var/frontends/loyaltyfe/*",
-            "post-setup": "npm install",
-            "pre-deploy": "pm2 delete Loyalty-Frontend",
-            "post-deploy": "npm run build && pm2 start npm --name Loyalty-Frontend -- start",
-        },
+  apps: [],
+  deploy: {
+    production: {
+      user: "admin-picare",
+      host: ["192.168.84.81"],
+      ref: "origin/main",
+      repo: "git@github.com:TriTruong666/Loyalty-FE.git",
+      path: "/var/frontends/loyaltyfe",
+      "pre-setup": "rm -r /var/frontends/loyaltyfe/*",
+      "post-setup": "npm install",
+      "pre-deploy":
+        "npm i && pm2 start npm --name Loyalty-Frontend -- start && pm2 delete Loyalty-Frontend",
+      "post-deploy":
+        "npm run build && pm2 start npm --name Loyalty-Frontend -- start",
     },
+  },
 };
-
