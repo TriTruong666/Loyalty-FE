971d00ec4d21f7548c960c0199dbca3f80987c7d
PhucTL <lethienphuc1009@gmail.com>
2025-02-24 10:17:50 +0700

    change location

--- src/app/dashboard/setting/location/page.tsx
+++ src/app/dashboard/setting/location/page.tsx
@@ -1,100 +1,25 @@
 "use client";
 import { useState } from "react";
 import NormalInput from "@/app/components/NormalInput";
-import Select from "@/app/components/Select";
-import { FaAddressCard, FaCity } from "react-icons/fa";
+import { Select, SelectItem } from "@heroui/react";
 import { FiSave } from "react-icons/fi";
+import { useAtom } from "jotai";
+import { dataCreateAccountState } from "@/app/store/accountAtoms";
+import {
+  useGetAllProvince,
+  useGetDistrictByProvince,
+  useGetWardByDistrict,
+} from "@/app/hooks/hook";
 
-// Add these types near the top of the file
-type ProvinceKey = "HN" | "HCM" | "DN";
-type DistrictKey =
-  | "BTL"
-  | "NTL"
-  | "CG"
-  | "HK"
-  | "Q1"
-  | "Q3"
-  | "Q5"
-  | "TB"
-  | "HC"
-  | "TK"
-  | "LC";
-
-const PROVINCES = [
-  { value: "HN", label: "Hà Nội" },
-  { value: "HCM", label: "Hồ Chí Minh" },
-  { value: "DN", label: "Đà Nẵng" },
-];
-
-const DISTRICTS: Record<ProvinceKey, { value: string; label: string }[]> = {
-  HN: [
-    { value: "BTL", label: "Bắc Từ Liêm" },
-    { value: "NTL", label: "Nam Từ Liêm" },
-    { value: "CG", label: "Cầu Giấy" },
-    { value: "HK", label: "Hoàn Kiếm" },
-  ],
-  HCM: [
-    { value: "Q1", label: "Quận 1" },
-    { value: "Q3", label: "Quận 3" },
-    { value: "Q5", label: "Quận 5" },
-    { value: "TB", label: "Tân Bình" },
-  ],
-  DN: [
-    { value: "HC", label: "Hải Châu" },
-    { value: "TK", label: "Thanh Khê" },
-    { value: "LC", label: "Liên Chiểu" },
-  ],
-};
-
-const WARDS: Record<DistrictKey, { value: string; label: string }[]> = {
-  BTL: [
-    { value: "XD", label: "Xuân Đỉnh" },
-    { value: "TP", label: "Thụy Phương" },
-    { value: "CD", label: "Cổ Nhuế" },
-  ],
-  NTL: [
-    { value: "MP", label: "Mỹ Đình" },
-    { value: "TD", label: "Tây Mỗ" },
-    { value: "PD", label: "Phú Đô" },
-  ],
-  CG: [{ value: "DG", label: "Dịch Vọng" }],
-  HK: [{ value: "HL", label: "Hàng Lược" }],
-  Q1: [{ value: "BN", label: "Bến Nghé" }],
-  Q3: [{ value: "1", label: "Phường 1" }],
-  Q5: [{ value: "2", label: "Phường 2" }],
-  TB: [{ value: "3", label: "Phường 3" }],
-  HC: [{ value: "TT", label: "Thanh Thuận" }],
-  TK: [{ value: "XH", label: "Xuân Hà" }],
-  LC: [{ value: "HM", label: "Hòa Minh" }],
-};
+import { IoIosPin } from "react-icons/io";
 
 export default function SettingPage() {
-  const [selectedProvince, setSelectedProvince] = useState<ProvinceKey | "">(
-    ""
-  );
-  const [selectedDistrict, setSelectedDistrict] = useState<DistrictKey | "">(
-    ""
-  );
-  const [selectedWard, setSelectedWard] = useState("");
-  const [address, setAddress] = useState("");
-
-  const handleProvinceChange = (value: string) => {
-    setSelectedProvince(value as ProvinceKey);
-    setSelectedDistrict("");
-    setSelectedWard("");
-  };
-
-  const handleDistrictChange = (value: string) => {
-    setSelectedDistrict(value as DistrictKey);
-    setSelectedWard("");
-  };
-
   return (
     <div className="flex flex-col px-[40px] py-[30px]">
       <div className="flex items-center justify-between">
         <div className="flex flex-col">
           <p className="text-[22px] font-light select-none">
-            Thông tin cá nhân
+            Địa chỉ khách hàng
           </p>
           <p className="text-[12px] text-normal">
             Cập nhật thông tin cá nhân của bạn
@@ -105,41 +30,147 @@ export default function SettingPage() {
           <p className="text-[12px] text-background">Lưu thông tin</p>
         </div>
       </div>
-      <div className="flex flex-col mt-[40px] gap-y-[20px]">
-        <NormalInput
-          label="Địa chỉ"
-          placeholder="123 nguyễn tri phương"
-          icon={<FaAddressCard size={20} />}
-          value={address}
-          onChange={(e) => setAddress(e.target.value)}
-        />
+      <LocationForm />
+    </div>
+  );
+}
+
+function LocationForm() {
+  const [submitData, setSubmitData] = useAtom(dataCreateAccountState);
+  const [selectedProvince, setSelectedProvince] = useState<string | null>(null);
+  const [selectedDistrict, setSelectedDistrict] = useState<string | null>(null);
+  const [selectedWard, setSelectedWard] = useState<string | null>(null);
+  const { data: provinces, isLoading } = useGetAllProvince();
+  const { data: districts } = useGetDistrictByProvince(
+    selectedProvince as string
+  );
+  const { data: wards } = useGetWardByDistrict(selectedDistrict as string);
+
+  const handleProvinceChange = (provinceCode: string) => {
+    setSubmitData({
+      ...submitData,
+      address: {
+        ...submitData.address,
+        provinceCode: provinceCode,
+      },
+    });
+    setSelectedProvince(provinceCode);
+    setSelectedDistrict(null);
+    setSelectedWard(null);
+  };
+  const handleDistrictChange = (districtCode: string) => {
+    setSubmitData({
+      ...submitData,
+      address: {
+        ...submitData.address,
+        districtCode: districtCode,
+      },
+    });
+    setSelectedDistrict(districtCode);
+    setSelectedWard(null);
+  };
+  const handleWardChange = (wardCode: string) => {
+    setSubmitData({
+      ...submitData,
+      address: {
+        ...submitData.address,
+        wardCode: wardCode,
+      },
+    });
+    setSelectedWard(wardCode);
+  };
+
+  return (
+    <div className="flex flex-col justify-center items-center gap-y-3 w-full">
+      <div className="flex flex-col w-full gap-y-2 font-inter mt-[20px]">
+        <label
+          htmlFor="date"
+          className="font-semibold text-sm 2xl:text-[12px] mb-1"
+        >
+          Tỉnh / Thành phố
+        </label>
         <Select
-          label="Thành phố"
-          placeholder="Chọn thành phố"
-          icon={<FaCity size={20} />}
-          options={PROVINCES}
-          value={selectedProvince}
-          onChange={handleProvinceChange}
-        />
-        <Select
-          label="Quận"
-          placeholder="Chọn quận"
-          icon={<FaAddressCard size={20} />}
-          options={selectedProvince ? DISTRICTS[selectedProvince] : []}
-          value={selectedDistrict}
-          onChange={handleDistrictChange}
-          disabled={!selectedProvince}
-        />
-        <Select
-          label="Phường"
-          placeholder="Chọn phường"
-          icon={<FaAddressCard size={20} />}
-          options={selectedDistrict ? WARDS[selectedDistrict] : []}
-          value={selectedWard}
-          onChange={(value) => setSelectedWard(value)}
-          disabled={!selectedDistrict}
-        />
+          aria-label="province"
+          isVirtualized
+          variant="underlined"
+          placeholder="Tỉnh / Thành phố"
+          isLoading={isLoading}
+          onSelectionChange={(keys) => {
+            const selectedKey = Array.from(keys)[0] as string;
+            handleProvinceChange(selectedKey);
+          }}
+        >
+          {(provinces ?? []).map((province) => (
+            <SelectItem value={province.code} key={province.code}>
+              {province.fullName}
+            </SelectItem>
+          ))}
+        </Select>
       </div>
+      <div className="flex flex-col w-full gap-y-2 font-inter">
+        <label
+          htmlFor="date"
+          className="font-semibold text-sm 2xl:text-[12px] mb-1"
+        >
+          Quận / Huyện
+        </label>
+        <Select
+          aria-label="district"
+          isVirtualized
+          variant="underlined"
+          placeholder="Quận / Huyện"
+          isDisabled={!selectedProvince}
+          onSelectionChange={(keys) => {
+            const selectedKey = Array.from(keys)[0] as string;
+            handleDistrictChange(selectedKey);
+          }}
+        >
+          {(districts ?? []).map((district) => (
+            <SelectItem value={district.code} key={district.code}>
+              {district.fullName}
+            </SelectItem>
+          ))}
+        </Select>
+      </div>
+      <div className="flex flex-col w-full gap-y-2 font-inter">
+        <label
+          htmlFor="date"
+          className="font-semibold text-sm 2xl:text-[12px] mb-1"
+        >
+          Phường / Xã
+        </label>
+        <Select
+          aria-label="ward"
+          isVirtualized
+          variant="underlined"
+          placeholder="Phường / Xã"
+          isDisabled={!selectedDistrict}
+          onSelectionChange={(keys) => {
+            const selectedKey = Array.from(keys)[0] as string;
+            handleWardChange(selectedKey);
+          }}
+        >
+          {(wards ?? []).map((ward) => (
+            <SelectItem value={ward.code} key={ward.code}>
+              {ward.fullName}
+            </SelectItem>
+          ))}
+        </Select>
+      </div>
+      <NormalInput
+        onChange={(e) =>
+          setSubmitData({
+            ...submitData,
+            address: {
+              ...submitData.address,
+              street: e.target.value,
+            },
+          })
+        }
+        label="Địa chỉ giao hàng"
+        placeholder="Vinhomes Grand Central Park"
+        icon={<IoIosPin size={20} />}
+      />
     </div>
   );
 }
