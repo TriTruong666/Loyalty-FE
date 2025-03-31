37e8c9c5ca5b2d9b001bacd8d453c4d5eb1146a8
TriTruong666 <tritruonghoang3@gmail.com>
2025-03-31 19:04:49 +0700

    Update Policy

--- src/app/policies/page.tsx
+++ src/app/policies/page.tsx
@@ -4,7 +4,7 @@ export default function PoliciesPage() {
   return (
     <div className="flex flex-col relative font-open w-full border-r border-neutral-700">
       <div className="flex justify-center">
-        <p className="text-[28px] font-bold">Giới thiệu PicareVN Loyalty</p>
+        <p className="text-[32px] font-bold">Giới thiệu PicareVN Loyalty</p>
       </div>
     </div>
   );
