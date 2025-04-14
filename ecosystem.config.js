c4ab76be4d0a93d508cee9ef54907f9d425e522a
Sakamoto2004 <phbtoan9185@gmail.com>
2025-04-14 17:12:27 +0700

    Test pnpm

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,7 +8,7 @@ module.exports = {
       repo: "git@github.com:TriTruong666/Loyalty-FE.git",
       path: "/var/frontends/loyaltyfe",
       "post-setup": "pnpm install --verbose && pnpm run build",
-      "pre-deploy": "pm2 start npm --name Loyalty-Frontend -- start && pm2 delete Loyalty-Frontend",
+      "pre-deploy": "pm2 start pnpm --name Loyalty-Frontend -- start && pm2 delete Loyalty-Frontend",
       "post-deploy": "pnpm i --verbose && pnpm run build && pm2 start pnpm --name Loyalty-Frontend -- start",
     },
   },
