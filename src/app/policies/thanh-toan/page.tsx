a5228cc7561c801be966f871a39003858e723638
TriTruong666 <tritruonghoang3@gmail.com>
2025-04-18 17:16:45 +0700

    Update Policy

--- src/app/policies/thanh-toan/page.tsx
+++ src/app/policies/thanh-toan/page.tsx
@@ -43,11 +43,32 @@ export default function ThanhtoanPolicyPage() {
         <h3 className="text-xl font-semibold mt-4 text-foreground">
           Đối hình thức công nợ
         </h3>
-        <p>
-          Đối với khách hàng tham gia Loyalty có ký hợp đồng, được gia hạn tối
-          đa <span className="font-bold text-primary">21 ngày</span> và không
-          quá <span className="font-bold text-primary">30 triệu đồng</span>.
-        </p>
+        <div className="flex flex-col gap-y-[15px] text-neutral-300">
+          <p>
+            Đối với khách hàng tham gia Loyalty có ký hợp đồng, được gia hạn tối
+            đa <span className="font-bold text-primary">21 ngày</span> và không
+            quá <span className="font-bold text-primary">30 triệu đồng</span>.
+          </p>
+          <p className="leading-7">
+            Hệ thống Loyalty sẽ kiểm tra nếu bạn đang có 1 đơn hàng{" "}
+            <span className="text-danger-500 font-semibold">Công nợ</span> thì
+            bạn sẽ không thể tiếp tục mua sắm. Để có thể tiếp tục mua tiếp thì
+            đơn hàng đó cần phải{" "}
+            <span className="font-bold text-success-400">
+              Thanh toán hoàn tất
+            </span>{" "}
+            và phải được{" "}
+            <span className="font-bold text-success-400">Giao thành công</span>.
+          </p>
+          <p>
+            Lưu ý: Nếu đơn hàng đó chưa kịp{" "}
+            <span className="font-bold text-success-400">
+              Thanh toán thành công
+            </span>{" "}
+            trong vòng 21 ngày thì đơn sẽ tự chuyển đổi trạng thái thành{" "}
+            <span className="font-bold text-danger-500">Đã huỷ</span>.
+          </p>
+        </div>
       </div>
     </div>
   );
